f87531039903c5c96e73dd7524e8cfe43f480650
kopalych <kopalychmail@gmail.com>
2025-02-07 00:32:08 +0100

    Final version

--- include/philosophers.h
+++ include/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: akostian <akostian@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 19:42:50 by akostian          #+#    #+#             */
-/*   Updated: 2025/02/05 01:59:01 by akostian         ###   ########.fr       */
+/*   Updated: 2025/02/07 00:21:18 by akostian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@
 
 # include "ANSI_colors.h"
 
-
 # ifndef COLORED
 #  define COLORED 0
 # endif
@@ -38,12 +37,14 @@
 #  define PHILO_SLEEPING_MSG "\033[1;34mis sleeping\n\033[0m"
 #  define PHILO_THINKING_MSG "\033[1;32mis thinking\n\033[0m"
 #  define PHILO_TAKING_MSG "\033[1;35mhas taken a fork\n\033[0m"
+#  define PHILO_DIED_MSG  "\033[1;30mdied\n\033[0m"
 # else
 #  define PHILO_N "%-4d"
 #  define PHILO_EATING_MSG "is eating\n"
 #  define PHILO_SLEEPING_MSG "is sleeping\n"
 #  define PHILO_THINKING_MSG "is thinking\n"
 #  define PHILO_TAKING_MSG "has taken a fork\n"
+#  define PHILO_DIED_MSG "died\n"
 # endif
 
 # define INCORRECT_ARGS_ERROR_MESSAGE \
@@ -67,6 +68,10 @@ typedef struct s_simulation_settings
 	unsigned int		ph_eat_count;	//nbr of times each philosopher must eat
 	pthread_mutex_t		*forks;
 	pthread_mutex_t		*print_mutex;
+	unsigned char		is_dead;
+	struct s_ph_data	*philo_data;
+	time_t				*last_meals;
+	unsigned char		*eat_enough;
 }	t_sim_settings;
 
 typedef struct s_ph_data
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: akostian <akostian@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 17:18:43 by akostian          #+#    #+#             */
-/*   Updated: 2025/02/05 01:55:42 by akostian         ###   ########.fr       */
+/*   Updated: 2025/02/07 00:19:31 by akostian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	philo_wait(t_ph_data *philo_data, unsigned int wait_time)
 	time_t	wake_up;
 
 	wake_up = time_diff(philo_data->start_time) + wait_time;
-	while (1)
+	while (!philo_data->sim->is_dead)
 	{
 		if (time_diff(philo_data->start_time) > wake_up)
 			return ;
@@ -46,21 +46,76 @@ void	*philosopher_thread(void *arg)
 
 	philo_data = *(t_ph_data *)arg;
 	eaten_times = 0;
-	while ((eaten_times < philo_data.sim->ph_eat_count)
-		|| !philo_data.sim->ph_eat_count)
+	philo_data.sim->last_meals[philo_data.philo_n - 1]
+		= time_diff(philo_data.start_time);
+	if (philo_data.sim->philo_n == 1)
+		philo_wait(&philo_data, philo_data.sim->time_to_die + 10);
+	while (1)
 	{
+		if (philo_data.sim->is_dead)
+			return (0);
 		pthread_mutex_lock(philo_data.left_fork);
+		if (philo_data.sim->is_dead)
+			return (pthread_mutex_unlock(philo_data.left_fork), NULL);
 		philo_print(&philo_data, PHILO_TAKING_MSG);
 		pthread_mutex_lock(philo_data.right_fork);
+		if (philo_data.sim->is_dead)
+		{
+			pthread_mutex_unlock(philo_data.left_fork);
+			pthread_mutex_unlock(philo_data.right_fork);
+			return (NULL);
+		}
 		philo_print(&philo_data, PHILO_TAKING_MSG);
 		philo_print(&philo_data, PHILO_EATING_MSG);
 		philo_wait(&philo_data, philo_data.sim->time_to_eat);
 		pthread_mutex_unlock(philo_data.left_fork);
 		pthread_mutex_unlock(philo_data.right_fork);
+		if (philo_data.sim->is_dead)
+			return (0);
 		eaten_times++;
+		if ((eaten_times >= philo_data.sim->ph_eat_count)
+			&& philo_data.sim->ph_eat_count)
+			philo_data.sim->eat_enough[philo_data.philo_n - 1] = 1;
+		philo_data.sim->last_meals[philo_data.philo_n - 1]
+			= time_diff(philo_data.start_time);
 		philo_print(&philo_data, PHILO_SLEEPING_MSG);
 		philo_wait(&philo_data, philo_data.sim->time_to_slp);
+		if (philo_data.sim->is_dead)
+			return (0);
 		philo_print(&philo_data, PHILO_THINKING_MSG);
+	}
+	return (0);
+}
+
+void	free_data(
+	t_sim_settings *sim,
+	pthread_t **threads
+)
+{
+	free(*threads);
+	free(sim->philo_data);
+	free(sim->forks);
+	free(sim->last_meals);
+	free(sim->eat_enough);
+}
+
+int	alloc_data(
+	t_sim_settings *sim,
+	pthread_t **threads,
+	t_ph_data **philo_data
+)
+{
+	*threads = ft_calloc(sim->philo_n + 1, sizeof(pthread_t));
+	*philo_data = ft_calloc(sim->philo_n, sizeof(t_ph_data));
+	sim->forks = ft_calloc(sim->philo_n, sizeof(pthread_mutex_t));
+	sim->last_meals = ft_calloc(sim->philo_n, sizeof(time_t));
+	sim->eat_enough = ft_calloc(sim->philo_n, sizeof(unsigned char));
+	sim->philo_data = *philo_data;
+	if (!*threads || !*philo_data
+		|| !sim->forks || !sim->last_meals || !sim->eat_enough)
+	{
+		free_data(sim, threads);
+		return (ENOMEM);
 	}
 	return (0);
 }
@@ -74,19 +129,16 @@ unsigned char	init_data(
 {
 	unsigned int	i;
 
-	*threads = ft_calloc(sim->philo_n, sizeof(pthread_t));
-	*philo_data = ft_calloc(sim->philo_n, sizeof(t_ph_data));
-	sim->forks = ft_calloc(sim->philo_n, sizeof(pthread_mutex_t));
-	if (!*threads || !*philo_data || !sim->forks)
-		return (free(*threads), free(*philo_data), free(sim->forks), 1);
+	if (alloc_data(sim, threads, philo_data))
+		return (ENOMEM);
 	if (pthread_mutex_init(print_mutex, NULL))
-		return (free(*threads), free(*philo_data), free(sim->forks), 1);
+		return (free_data(sim, threads), 1);
 	sim->print_mutex = print_mutex;
 	i = -1;
 	while (++i < sim->philo_n)
 	{
 		if (pthread_mutex_init(&(sim->forks[i]), NULL))
-			return (free(*threads), free(*philo_data), free(sim->forks), 1);
+			return (free_data(sim, threads), 1);
 		(*philo_data)[i].philo_n = i + 1;
 		(*philo_data)[i].print_mutex = sim->print_mutex;
 		(*philo_data)[i].sim = sim;
@@ -96,7 +148,7 @@ unsigned char	init_data(
 			(*philo_data)[i].right_fork = &(sim->forks[i - 1]);
 		(*philo_data)[i].left_fork = &(sim->forks[i]);
 	}
-	return (0);
+	return (sim->is_dead = 0, sim->philo_data = *philo_data, 0);
 }
 
 unsigned char	create_philos(
@@ -132,21 +184,55 @@ unsigned char	create_philos(
 	return (0);
 }
 
+void	*reaper_thread(void *arg)
+{
+	t_sim_settings	*sim;
+	unsigned int	i;
+	unsigned int	j;
+
+	sim = (t_sim_settings *)arg;
+	while (1)
+	{
+		i = -1;
+		while (++i < sim->philo_n)
+		{
+			j = -1;
+			while (++j < sim->philo_n)
+				if (!sim->eat_enough[j])
+					break ;
+			if (j == sim->philo_n)
+				return (sim->is_dead = 1, NULL);
+			if ((time_diff(sim->philo_data[i].start_time) - sim->last_meals[i])
+				> sim->time_to_die)
+				return (philo_print(&(sim->philo_data[i]), PHILO_DIED_MSG),
+					sim->is_dead = 1, NULL);
+		}
+		usleep(500);
+	}
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_sim_settings		sim_settings;
 	pthread_t			*threads;
 	t_ph_data			*philosopher_data;
 	pthread_mutex_t		print_mutex;
+	unsigned int		i;
 
 	if (parse(argc, argv, &sim_settings))
 		return (printf(INCORRECT_ARGS_ERROR_MESSAGE), 1);
 	if (init_data(&sim_settings, &threads, &philosopher_data, &print_mutex))
 		return (printf(NO_MEMORY_ERROR_MESSAGE), ENOMEM);
 	if (create_philos(sim_settings, threads, philosopher_data))
-		return (printf(NO_MEMORY_ERROR_MESSAGE), ENOMEM);
-	for (unsigned int i = 0; i < sim_settings.philo_n; i++)
-		pthread_join(threads[i], NULL);
-	// printf("ms All threads are done.\n");
-	return (free(threads), free(philosopher_data), free(sim_settings.forks), 0);
+		return (free_data(&sim_settings, &threads),
+			printf(NO_MEMORY_ERROR_MESSAGE), ENOMEM);
+	usleep(2000);
+	if (pthread_create(&threads[sim_settings.philo_n], NULL,
+			reaper_thread, &sim_settings) != 0)
+		return (free_data(&sim_settings, &threads), 1);
+	i = 0;
+	while (i < sim_settings.philo_n + 1)
+		pthread_join(threads[i++], NULL);
+	return (free_data(&sim_settings, &threads), 0);
 }
